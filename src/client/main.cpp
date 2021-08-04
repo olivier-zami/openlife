3a7c4a14978208edd4f6ae7a5127923b736f63bc
olivier <olivier@lain-1.0>
2021-08-04 08:43:27 +0200

    delete --cached submodule + add src/client/main.cpp

---
+++ src/client/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by olivier on 04/08/2021.
+//
+int mainFunction( int inArgCount, char **inArgs );
+
+int main( int inArgCount, char **inArgs )
+{
+	return mainFunction( inArgCount, inArgs );
+}
