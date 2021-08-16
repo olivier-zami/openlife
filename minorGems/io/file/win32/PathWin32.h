d04c62b813703d37040b9cfc16e5e6a78fe1a3b7
olivier <olivier@lain-1.0>
2021-08-16 03:08:05 +0200

    filetree modification

--- minorGems/io/file/win32/PathWin32.h
+++ minorGems/io/file/win32/PathWin32.h
@@ -6,6 +6,6 @@
 #define ONELIFETEST_PATHWIN32_H
 
 #define INCLUDED_FILE "minorGems/io/file/Path.h"
-#include "src/system/_macro/win32_code_declaration.h"
+#include "src/system/_base/_macro/win32_code_declaration.h"
 
 #endif //ONELIFETEST_PATHWIN32_H
