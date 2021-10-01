ae1534c9783744c58a3c393ee5b424fb53a031bb
olivier <olivier@lain-1.0>
2021-10-01 22:28:50 +0200

    class log

---
+++ src/system/_base/log.cpp
@@ -0,0 +1,18 @@
+//
+// Created by olivier on 01/10/2021.
+//
+
+#include "log.h"
+
+#include <cstdio>
+
+void openLife::system::Log::notice(const char *message, ...)
+{
+	unsigned int bSize = 512;
+	char buffer[bSize];
+	va_list args;
+	va_start (args, message);
+	vsnprintf (buffer, bSize, message, args);
+	printf("\n===>%s", buffer);
+	va_end (args);
+}
---
+++ src/system/_base/log.h
@@ -0,0 +1,20 @@
+//
+// Created by olivier on 01/10/2021.
+//
+
+#ifndef OPENLIFE_LOG_H
+#define OPENLIFE_LOG_H
+
+#include <cstdarg>
+
+namespace openLife::system
+{
+	class Log
+	{
+		public:
+			static void notice(const char* message, ...);
+	};
+
+}
+
+#endif //OPENLIFE_LOG_H
