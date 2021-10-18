cddf65f3405d0dde914247330f657af7d568c8ec
olivier <olivier@lain-1.0>
2021-10-19 01:19:42 +0200

    add Home location methods in procedure/nation.cpp

--- src/client/procedure/nation.cpp
+++ src/client/procedure/nation.cpp
@@ -5,17 +5,124 @@
 #include "nation.h"
 
 #include "minorGems/util/SimpleVector.h"
+#include "minorGems/game/game.h"
 
 SimpleVector<Homeland> homelands;
+SimpleVector<HomePos> homePosStack;
+SimpleVector<HomePos> oldHomePosStack;
 
 Homeland *getHomeland( int inCenterX, int inCenterY )
 {
-	for( int i=0; i<homelands.size(); i++ ) {
+	for( int i=0; i<homelands.size(); i++ )
+	{
 		Homeland *h = homelands.getElement( i );
 
-		if( h->x == inCenterX && h->y == inCenterY ) {
+		if( h->x == inCenterX && h->y == inCenterY )
+		{
 			return h;
 		}
 	}
 	return NULL;
+}
+
+void processHomePosStack()
+{
+	int num = homePosStack.size();
+	if( num > 0 ) {
+		HomePos *r = homePosStack.getElement( num - 1 );
+
+		if( r->temporary && r->temporaryExpireETA != 0 &&
+			game_getCurrentTime() > r->temporaryExpireETA ) {
+			// expired
+			homePosStack.deleteElement( num - 1 );
+		}
+	}
+}
+
+HomePos *getHomePosRecord()
+{
+	processHomePosStack();
+
+	int num = homePosStack.size();
+	if( num > 0 ) {
+		return homePosStack.getElement( num - 1 );
+	}
+	else {
+		return NULL;
+	}
+}
+
+// returns pointer to record, NOT destroyed by caller, or NULL if
+// home unknown
+GridPos *getHomeLocation(
+	char *outTemp, char *outTempPerson,
+	const char **outTempPersonKey,
+	char inAncient )
+{
+	*outTemp = false;
+
+	if( inAncient ) {
+		GridPos *returnPos = NULL;
+
+		if( homePosStack.size() > 0 ) {
+			HomePos *r = homePosStack.getElement( 0 );
+			if( r->ancient ) {
+				returnPos = &( r->pos );
+			}
+		}
+		return returnPos;
+	}
+
+
+	HomePos *r = getHomePosRecord();
+
+	// don't consider ancient marker here, if it's the only one
+	if( r != NULL && ! r->ancient ) {
+		*outTemp = r->temporary;
+		*outTempPerson = r->tempPerson;
+		*outTempPersonKey = r->tempPersonKey;
+		return &( r->pos );
+	}
+	else {
+		return NULL;
+	}
+}
+
+void removeHomeLocation( int inX, int inY )
+{
+	for( int i=0; i<homePosStack.size(); i++ ) {
+		GridPos p = homePosStack.getElementDirect( i ).pos;
+
+		if( p.x == inX && p.y == inY ) {
+			homePosStack.deleteElement( i );
+			break;
+		}
+	}
+}
+
+void removeAllTempHomeLocations()
+{
+	for( int i=0; i<homePosStack.size(); i++ )
+	{
+		if( homePosStack.getElementDirect( i ).temporary )
+		{
+			homePosStack.deleteElement( i );
+			i --;
+			break;
+		}
+	}
+}
+
+void addHomeLocation( int inX, int inY )
+{
+	removeAllTempHomeLocations();
+
+	removeHomeLocation( inX, inY );
+	GridPos newPos = { inX, inY };
+	HomePos p;
+	p.pos = newPos;
+	p.ancient = false;
+	p.temporary = false;
+
+	homePosStack.push_back( p );
 }
--- src/client/procedure/nation.h
+++ src/client/procedure/nation.h
@@ -2,8 +2,10 @@
 // Created by olivier on 18/10/2021.
 //
 
-#ifndef OPENLIFE_NATION_H
-#define OPENLIFE_NATION_H
+#ifndef OPENLIFE_CLIENT_PROCEDURE_NATION_H
+#define OPENLIFE_CLIENT_PROCEDURE_NATION_H
+
+#include "OneLife/gameSource/GridPos.h"
 
 typedef struct Homeland
 {
@@ -11,6 +13,28 @@ typedef struct Homeland
 	char *familyName;
 } Homeland;
 
-Homeland *getHomeland( int inCenterX, int inCenterY );
+// most recent home at end
+typedef struct {
+	GridPos pos;
+	char ancient;
+	char temporary;
+	char tempPerson;
+	int personID;
+	const char *tempPersonKey;
+	// 0 if not set
+	double temporaryExpireETA;
+} HomePos;
 
-#endif //OPENLIFE_NATION_H
+Homeland *getHomeland( int inCenterX, int inCenterY );
+void processHomePosStack();
+HomePos *getHomePosRecord();
+GridPos *getHomeLocation(
+	char *outTemp,
+	char *outTempPerson,
+	const char **outTempPersonKey,
+	char inAncient );
+void removeHomeLocation( int inX, int inY );
+void removeAllTempHomeLocations();
+void addHomeLocation( int inX, int inY );
+
+#endif //OPENLIFE_CLIENT_PROCEDURE_NATION_H
